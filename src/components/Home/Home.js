bf4459563c182726774205fa08e304299277685c
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-06-23 18:54:24 +0600

    refactor: update home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -163,19 +163,16 @@ const Home = () => {
               )}
             </div>
           </div>
-          {/* {(isLoading || isLoading) && <RepoSpinner />} */}
           <div className="repo-container">
-            {isLoading ? (
-              <>
-                <Skeleton type="rectangle" height="100px" />
-                <Skeleton type="rectangle" height="100px" />
-                <Skeleton type="rectangle" height="100px" />
-              </>
-            ) : (
-              repoData?.map((repo, index) => <Repo key={index} repo={repo} />)
-            )}
-            {/* {!isLoading &&
-              repoData?.map((repo, index) => <Repo key={index} repo={repo} />)} */}
+            {isLoading
+              ? Array.from({ length: 5 }).map((_, index) => (
+                  <>
+                    <Skeleton type="rectangle" key={index} height="100px" />
+                  </>
+                ))
+              : repoData?.map((repo, index) => (
+                  <Repo key={index} repo={repo} />
+                ))}
           </div>
           {pageCount >= 1 && (
             <div className="pagination-bar">
@@ -207,179 +204,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// import React, { useEffect, useState } from "react";
-// import "./Home.css";
-// import Repo from "./Repo";
-// import ReactPaginate from "react-paginate";
-// import axios from "axios";
-// import Spinner from "../Spinner/Spinner";
-// import RepoSpinner from "./RepoSpinner/RepoSpinner";
-// import { FiLink2 } from "react-icons/fi";
-// import { MdLocationOn } from "react-icons/md";
-
-// const Home = () => {
-//   const [pageCount, setPageCount] = useState(0);
-//   const [repodata, setRepoData] = useState(null);
-//   const [numberOfSelectPage, setNumberOfSelectPage] = useState(1);
-//   const [accountData, setAccountData] = useState({});
-//   const [accountLoading, setAccountLoading] = useState(true);
-//   const [reposLoading, setReposLoading] = useState(true);
-//   const [username, setUsername] = useState("tanvir-alam625");
-//   const [error, setError] = useState(false);
-//   //  load the repo data
-//   useEffect(() => {
-//     const getGithubRepoApi = async () => {
-//       try {
-//         setReposLoading(true);
-//         const res = await fetch(
-//           `https://api.github.com/users/${username}/repos?page=${numberOfSelectPage}&per_page=10`
-//         );
-//         console.log(res.status);
-//         if (res.status !== 200) {
-//           setError(true);
-//         } else {
-//           setError(false);
-//           const data = await res.json();
-//           const dataLimit = await res.headers.get("x-ratelimit-limit");
-//           setPageCount(Math.ceil(dataLimit / 10));
-//           setRepoData(data);
-//           setReposLoading(false);
-//         }
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     getGithubRepoApi();
-//   }, [numberOfSelectPage, username]);
-
-//   const deBouncer = (func, delay) => {
-//     let timer;
-//     return function (...args) {
-//       if (timer) clearTimeout(timer);
-//       timer = setTimeout(() => func(...args), delay);
-//     };
-//   };
-
-//   //  get account  function
-//   const getAccountData = async (user) => {
-//     try {
-//       const { data } = await axios.get(`https://api.github.com/users/${user}`);
-//       await setAccountData(data);
-//       await setAccountLoading(false);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   getAccountData(username);
-//   // pagination function
-//   const handlePageClick = (data) => {
-//     setNumberOfSelectPage(data.selected + 1);
-//   };
-
-//   if (accountLoading) {
-//     return <Spinner />;
-//   }
-
-//   const handleOnChange = deBouncer((e) => {
-//     setUsername(e.target.value);
-//     getAccountData(e.target.value);
-//   }, 1000);
-
-//   return (
-//     <main className="home-container">
-//       <div
-//         style={{ display: "flex", justifyContent: "center", margin: "20px" }}
-//       >
-//         <input
-//           type="text"
-//           name="search"
-//           id="search"
-//           onChange={handleOnChange}
-//           style={{
-//             padding: "10px 25px",
-//             outline: "none",
-//             border: "2px solid #428BCA",
-//             width: "70%",
-//           }}
-//           placeholder="Type GitHub Username"
-//         />
-//       </div>
-//       {error ? (
-//         <h2>Not Found User</h2>
-//       ) : (
-//         <>
-//           <div className="home-heading">
-//             <div className="img-container">
-//               <img
-//                 src={accountData?.avatar_url}
-//                 alt="img"
-//                 className="account-img"
-//               />
-//               <p className="gitHub-link">
-//                 <a
-//                   href={`https://twitter.com/${accountData?.twitter_username}`}
-//                   target="_blank"
-//                 >
-//                   <FiLink2
-//                     size="1.4rem"
-//                     color="#000"
-//                     style={{ margin: "10px" }}
-//                   />
-//                 </a>
-//                 <span>{accountData?.html_url}</span>
-//               </p>
-//             </div>
-//             <div className="account-info">
-//               <h2>{accountData?.name}</h2>
-//               <p className="desc">{accountData?.bio}</p>
-//               <p className="location">
-//                 <MdLocationOn
-//                   size="1.4rem"
-//                   color="#000"
-//                   style={{ margin: "10px" }}
-//                 />
-//                 <span>{accountData?.location}</span>
-//               </p>
-//               <p>
-//                 Twitter: https://twitter.com/{accountData?.twitter_username}
-//               </p>
-//             </div>
-//           </div>
-//           {(accountLoading || reposLoading) && <RepoSpinner />}
-
-//           <div className="repo-container">
-//             {!reposLoading &&
-//               repodata?.map((repo, index) => <Repo key={index} repo={repo} />)}
-//           </div>
-
-//           {/* pagination code  */}
-//           {pageCount >= 1 && (
-//             <div className="pagination-bar">
-//               <ReactPaginate
-//                 previousLabel={`<<`}
-//                 nextLabel={`>>`}
-//                 pageCount={pageCount}
-//                 pageRangeDisplayed={2}
-//                 marginPagesDisplayed={2}
-//                 breakLabel={"****"}
-//                 onPageChange={handlePageClick}
-//                 containerClassName={"pagination"}
-//                 pageClassName={"page-item"}
-//                 pageLinkClassName={"page-link"}
-//                 nextClassName={"page-item"}
-//                 nextLinkClassName={"page-link"}
-//                 previousClassName={"page-item"}
-//                 previousLinkClassName={"page-link"}
-//                 breakClassName={"page-item"}
-//                 breakLinkClassName={"page-link"}
-//                 activeClassName={"active"}
-//               />
-//             </div>
-//           )}
-//         </>
-//       )}
-//     </main>
-//   );
-// };
-// export default Home;
